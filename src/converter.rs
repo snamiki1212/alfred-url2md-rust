6a3fcf150dfa19fc900f85a41779d968fad0781e
snamiki1212 <snamiki1212@gmail.com>
2021-10-17 02:14:16 -0700

    feat: add parser

--- src/converter.rs
+++ src/converter.rs
@@ -1,6 +1,21 @@
+use scraper::{Html, Selector};
+
 pub async fn url2md(url: &String) -> Result<String, reqwest::Error> {
+    // fetch
     let _url = "https://www.rust-lang.org";
-    let title = reqwest::get(_url).await?.text().await?;
-    println!("title is {}", title);
+    let body = reqwest::get(_url).await?.text().await?;
+    println!("title is {:?}", body);
+
+    // parser
+    let html = Html::parse_document(body.as_str());
+    let root_ref = html.root_element();
+    let title = root_ref
+        .select(&Selector::parse("title").unwrap())
+        .next()
+        .unwrap()
+        .inner_html();
+
+    // serialize
+    println!("in url md: title={:?}", title);
     Ok("[md](google.com)".to_string())
 }
