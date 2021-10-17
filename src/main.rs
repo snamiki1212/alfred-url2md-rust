8ad7f16b13ad39ccae0ff9aed926b82e87e5a6c3
snamiki1212 <snamiki1212@gmail.com>
2021-10-17 01:24:30 -0700

    feat: add cli template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::{AppSettings, Clap};
+
+#[derive(Clap)]
+#[clap(
+    version = "1.0",
+    author = "Shun Namiki aka Nash <snamiki1212@gmail.com>"
+)]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Opts {
+    input: String,
+}
 fn main() {
-    println!("Hello, world!");
+    let opts: Opts = Opts::parse();
+    let md = url2md(&opts.input);
+    println!("{}", md);
+}
+
+fn url2md(url: &String) -> String {
+    "[md](google.com)".to_string()
 }
